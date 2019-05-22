93bff3e12ff59d5eb8cdf144a362a11e7040a144
Henrik Häggbom <hhaggbom@gmail.com>
2019-05-22 15:26:30 +0200

    All tests passed

---
+++ src/SolidFeeCalculator/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SolidFeeCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var fee = 0;
+            int usertype = 1, itemtype = 1, itemprice = 100;
+            DateTime itemenddate = DateTime.Today;
+
+            try
+            {
+                if(args.Length > 0)
+                    usertype = int.Parse(args[0]);
+                if(args.Length > 1)
+                    itemtype = int.Parse(args[1]);
+                if(args.Length > 2)
+                    itemprice = int.Parse(args[2]);
+                if(args.Length > 3)
+                    itemenddate = DateTime.Parse(args[3]);
+                fee = CalculateFee(usertype, itemtype, itemprice, itemenddate);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+
+            Console.WriteLine(fee.ToString());
+            Console.Read();
+        }
+
+        /// <summary>
+        ///   This function handles all calculation you ever need!
+        /// </summary>
+        /// <param name="usertype"> 0= Normal, 1 = Company</param>
+        /// <param name="itemtype"> 0= Auction, 1 = BuyItNow</param>
+        /// <param name="itemprice"></param>
+        /// <param name="itemenddate">Time Item ends </param>
+        /// <returns></returns>
+        public static int CalculateFee(int usertype, int itemtype, int itemprice,  DateTime itemenddate)
+        {
+            try
+            {
+                decimal percentage;
+
+                switch (usertype)
+                {
+                    case 0: //Normal
+                         percentage = 0.07m;
+                        #region Normal user
+                        int itemfeeN = Decimal.ToInt32(itemprice * percentage);
+                        if (itemtype == 0) //Auction
+                        {
+                            var enddateDiscount = 0;
+                            if (itemenddate == DateTime.Today) enddateDiscount = 10;
+
+                            return itemfeeN + 25 - enddateDiscount;
+                        }
+                        else if (itemtype == 1) //BuyItNow
+                        {
+                            var enddateDiscount = 0;
+                            if (itemenddate == DateTime.Today) enddateDiscount = 10;
+
+                            return itemfeeN + 35 - enddateDiscount;
+                        }
+                        break; 
+	                    #endregion
+                    case 1: //Company
+                        percentage = 0.04m;
+                        #region Company
+                        int itemfee = Decimal.ToInt32(itemprice * percentage);
+                        if (itemtype == 0) //Auction
+                        {
+                            if (itemenddate == DateTime.Today)
+                                return itemfee + 25 - 15;// Enddate discount and company discount
+
+                            return itemfee + 25 - 5;// Only company discount
+                        }
+                        else if (itemtype == 1) //BuyItNow
+                        {
+                            if (itemenddate == DateTime.Today)
+                                return itemfee + 35 - 15;// Enddate discount and company discount
+
+                            return itemfee + 35 - 5;// Only company discount
+                        }
+                        break; 
+                        #endregion
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/SolidFeeCalculatorTest/ProgramTest.cs
@@ -0,0 +1,116 @@
+﻿using NUnit.Framework;
+using SolidFeeCalculator;
+using System;
+
+namespace SolidFeeCalculatorTest
+{
+    
+    
+    [TestFixture]
+    public class ProgramTest
+    {
+        #region Normal User
+        [Test]
+        public void CalculateNormalAuctionFeeEndDateTodayTest()
+        {
+            int userType = 0; // TODO: Initialize to an appropriate value
+            int itemType = 0; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today; // TODO: Initialize to an appropriate value
+            int expected = 22; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void CalculateNormalBuyNowEndDateTodayFeeTest()
+        {
+            int userType = 0; // TODO: Initialize to an appropriate value
+            int itemType = 1; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today; // TODO: Initialize to an appropriate value
+            int expected = 32; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CalculateNormalAuctionFeeEndDateTomorrowTest()
+        {
+            int userType = 0; // TODO: Initialize to an appropriate value
+            int itemType = 0; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today.AddDays(1); // TODO: Initialize to an appropriate value
+            int expected = 32; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void CalculateNormalBuyNowEndDateTomorrowFeeTest()
+        {
+            int userType = 0; // TODO: Initialize to an appropriate value
+            int itemType = 1; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today.AddDays(1); // TODO: Initialize to an appropriate value
+            int expected = 42; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+        #region Company User
+        [Test]
+        public void CalculateCompUserAuctionFeeEndDateTodayTest()
+        {
+            int userType = 1; // TODO: Initialize to an appropriate value
+            int itemType = 0; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today; // TODO: Initialize to an appropriate value
+            int expected = 14; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void CalculatewCompUserBuyNowEndDateTodayFeeTest()
+        {
+            int userType = 1; // TODO: Initialize to an appropriate value
+            int itemType = 1; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today; // TODO: Initialize to an appropriate value
+            int expected = 24; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CalculateCompUserAuctionFeeEndDateTomorrowTest()
+        {
+            int userType = 1; // TODO: Initialize to an appropriate value
+            int itemType = 0; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today.AddDays(1); // TODO: Initialize to an appropriate value
+            int expected = 24; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void CalculateCompUserBuyNowEndDateTomorrowFeeTest()
+        {
+            int userType = 1; // TODO: Initialize to an appropriate value
+            int itemType = 1; // TODO: Initialize to an appropriate value
+            int itemPrice = 100; // TODO: Initialize to an appropriate value
+            DateTime itemEndDate = DateTime.Today.AddDays(1); // TODO: Initialize to an appropriate value
+            int expected = 34; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = Program.CalculateFee(userType, itemType, itemPrice, itemEndDate);
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
